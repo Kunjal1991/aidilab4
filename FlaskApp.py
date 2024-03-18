0e43cac4c63f8737c410b74c2e45d320e2a7aa28
KUNJAL BRAHMBHATT <110318018+Kunjal1991@users.noreply.github.com>
2024-03-18 13:03:04 -0400

    Add files via upload

---
+++ FlaskApp.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pickle
+from flask import Flask, request
+from flask import Flask, request, jsonify, render_template
+
+app=Flask(__name__)
+pickle_in = open("classifier.pkl","rb")
+classifier=pickle.load(pickle_in)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+
+@app.route('/predict',methods=['POST'])
+def predict():
+  
+    int_features = [x for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    print("final_features",final_features)
+
+    prediction = classifier.predict(final_features)
+    
+    
+    return render_template('index.html', prediction_text='The fish belong to species {}'.format(prediction))
+    
+    
+
+
+if __name__=='__main__':
+    app.run()
